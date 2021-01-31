cd96f320ad8af1979cb32e08c780dee1158b5366
dambar <notdambar@gmail.com>
2021-01-31 10:48:38 +0800

    updated 2020/01/31

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,95 @@
-console.log('Its working')
-let theme = localStorage.getItem('theme')
+let theme = localStorage.getItem('theme');
 
-if(theme == null){
-	setTheme('light')
-}else{
-	setTheme(theme)
+if (theme == null) {
+  setTheme('light');
+} else {
+  setTheme(theme);
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+let themeDots = document.getElementsByClassName('theme-dot');
 
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener('click', function () {
+    let mode = this.dataset.mode;
+    setTheme(mode, true);
+  });
 
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
+  themeDots[i].addEventListener('mouseover', function () {
+    let mode = this.dataset.mode;
+    setTheme(mode, false);
+  });
+
+  themeDots[i].addEventListener('mouseleave', function () {
+    setTheme(theme, false);
+  });
 }
 
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = '/public/css/default.css'
-	}
+function setTheme(mode, save) {
+  if (mode == 'light') {
+    document.getElementById('theme-style').href = '/public/css/default.css';
+  }
 
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = '/public/css/blue.css'
-	}
+  if (mode == 'blue') {
+    document.getElementById('theme-style').href = '/public/css/blue.css';
+  }
 
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = '/public/css/green.css'
-	}
+  if (mode == 'green') {
+    document.getElementById('theme-style').href = '/public/css/green.css';
+  }
 
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = '/public/css/purple.css'
-	}
+  if (mode == 'purple') {
+    document.getElementById('theme-style').href = '/public/css/purple.css';
+  }
 
-	localStorage.setItem('theme', mode)
+  if (save) {
+    localStorage.setItem('theme', mode);
+    theme = localStorage.getItem('theme');
+  }
 }
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: '.ml5 .line',
+    opacity: [0.5, 1],
+    scaleX: [0, 1],
+    easing: 'easeInOutExpo',
+    duration: 700,
+  })
+  .add({
+    targets: '.ml5 .line',
+    duration: 600,
+    easing: 'easeOutExpo',
+    translateY: (el, i) => -0.625 + 0.625 * 2 * i + 'em',
+  })
+  .add({
+    targets: '.ml5 .ampersand',
+    opacity: [0, 1],
+    scaleY: [0.5, 1],
+    easing: 'easeOutExpo',
+    duration: 600,
+    offset: '-=600',
+  })
+  .add({
+    targets: '.ml5 .letters-left',
+    opacity: [0, 1],
+    translateX: ['0.5em', 0],
+    easing: 'easeOutExpo',
+    duration: 600,
+    offset: '-=300',
+  })
+  .add({
+    targets: '.ml5 .letters-right',
+    opacity: [0, 1],
+    translateX: ['-0.5em', 0],
+    easing: 'easeOutExpo',
+    duration: 600,
+    offset: '-=600',
+  })
+  .add({
+    targets: '.ml5',
+    opacity: 0,
+    duration: 1000,
+    easing: 'easeOutExpo',
+    delay: 1000,
+  });
